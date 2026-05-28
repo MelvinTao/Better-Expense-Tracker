d8922331368b3e94a2cdee03795cf31aaa423b7c
Melvin <melvintaolife@gmail.com>
2026-05-27 20:57:42 -0700

    Initial Commit

---
+++ Better Expense Tracker/Better_Expense_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Better_Expense_TrackerApp.swift
+//  Better Expense Tracker
+//
+//  Created by Melvin Tao on 2026-05-27.
+//
+
+import SwiftUI
+
+@main
+struct Better_Expense_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Better Expense TrackerTests/Better_Expense_TrackerTests.swift
@@ -0,0 +1,16 @@
+//
+//  Better_Expense_TrackerTests.swift
+//  Better Expense TrackerTests
+//
+//  Created by Melvin Tao on 2026-05-27.
+//
+
+import Testing
+
+struct Better_Expense_TrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
